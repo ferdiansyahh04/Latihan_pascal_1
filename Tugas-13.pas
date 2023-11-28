af4f89aa62305da6dc2e8e272295945cf8b9ee94
Rizky Ferdiansyah <52203428+ferdiansyahh04@users.noreply.github.com>
2023-11-29 00:43:03 +0700

    Update Tugas-13.pas

--- Tugas-13.pas
+++ Tugas-13.pas
@@ -1,52 +1,35 @@
-program tiga_bilangan_terbesar;
+program tugas_13;
 uses crt;
 var
     Benar, Salah, Jumlah_soal, Jawaban, counter : integer;
     a, b : integer;
     nilai : real;
-    confirm, : char;
+    ulang : char;
 begin
     clrscr;
-    
+    repeat
     randomize;
     Benar := 0;
     Salah := 0;
     write('Input Jumlah Soal : ');
     readln(Jumlah_soal);
     for counter := 1 to Jumlah_soal do 
-    begin
-        a := random(100);
-        b := random(100);
-        write('Soal ', counter , ' : ', a , ' + ', b, ' = ');
-        readln(Jawaban);
-        if(Jawaban = (a + b)) then
+        begin
+            a := random(100);
+            b := random(100);
+                write('Soal ', counter , ' : ', a , ' + ', b, ' = ');
+                readln(Jawaban);
+                if(Jawaban = (a + b)) then
                 Benar := Benar + 1
-        else
+                else
                 Salah := Salah + 1;
-    end;
+        end;
     nilai := Benar / Jumlah_soal + 100;
     writeln('Jumlah Jawaban Benar : ', Benar);
     writeln('Jumlah Jawaban Salah : ', Salah);
     writeln('Nilai                : ', nilai:0:0);
 
-    writeln('Apakah Anda Ingin Mengulang (y/n)');
-    readln(confirm);
-    if confirm = 'y' then
-        begin 
-        a := random(100);
-        b := random(100);
-        write('Soal ', counter , ' : ', a , ' + ', b, ' = ');
-        readln(Jawaban);
-        if(Jawaban = (a + b)) then
-                Benar := Benar + 1
-        else
-                Salah := Salah + 1;
-        end;
-        nilai := Benar / Jumlah_soal + 100;
-        writeln('Jumlah Jawaban Benar : ', Benar);
-        writeln('Jumlah Jawaban Salah : ', Salah);
-        writeln('Nilai                : ', nilai:0:0);
-    
-    if confirm = 'n' then
-        exit
+    write('Apakah anda ingin mengulang? (y/n) : ');
+    readln(ulang);
+    until (ulang = 'n') or (ulang = 'N');
 end.
